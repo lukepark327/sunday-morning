7978a6821a249c1f37734a6bd2264bcfab8d6936
twodude <twodude@naver.com>
2018-12-19 17:07:52 +0900

    add live commerce function.

--- work/contracts/sunday_morning/live_broadcast.py
+++ work/contracts/sunday_morning/live_broadcast.py
@@ -3,6 +3,8 @@ from iconservice import *
 
 TAG = 'LiveBroadcast'
 
+HASH_NULL = "0x0000000000000000000000000000000000000000000000000000000000000000"
+
 
 # An interface of ICON Token Standard, IRC-2
 class TokenStandard(ABC):
@@ -42,19 +44,27 @@ class TokenFallbackInterface(InterfaceScore):
 
 class LiveBroadcast(IconScoreBase, TokenStandard):
 
-    _VIDEOS         = 'videos'
     _BALANCES       = 'balances'
     _TOTAL_SUPPLY   = 'total_supply'
     _DECIMALS       = 'decimals'
+
     _ADMIN          = 'admin'
+    _VIDEOS         = 'videos'
+    _ADRS           = 'adrs'
+    _AD_VIDEO       = 'ad_video'        # key: SHA256(AD) -> value: SHA256(VIDEO)
+
+
 
     def __init__(self, db: IconScoreDatabase) -> None:
         super().__init__(db)
         self._total_supply  = VarDB(self._TOTAL_SUPPLY, db, value_type=int)
         self._decimals      = VarDB(self._DECIMALS, db, value_type=int)
         self._balances      = DictDB(self._BALANCES, db, value_type=int)
-        self._videos        = DictDB(self._VIDEOS, db, value_type=Address)  # hash(SHA256): str -> Address
+
         self._admin         = VarDB(self._ADMIN, db, value_type=Address)
+        self._videos        = DictDB(self._VIDEOS, db, value_type=Address)  # hash(SHA256): str -> Address
+        self._adrs          = DictDB(self._ADRS, db, value_type=Address)    # hash(SHA256): str -> Address
+        self._ad_video      = DictDB(self._AD_VIDEO, db, value_type=str)    # hash(SHA256): str -> hash(SHA256): str
 
     def on_install(self, _initialSupply: int, _decimals: int) -> None:
         super().on_install()
@@ -73,8 +83,11 @@ class LiveBroadcast(IconScoreBase, TokenStandard):
         self._total_supply.set(total_supply)
         self._decimals.set(_decimals)
         self._balances[self.msg.sender] = total_supply
-        self._videos["0x0000000000000000000000000000000000000000000000000000000000000000"] = self.msg.sender
-        self._admin = self.msg.sender
+
+        self._admin                 = self.msg.sender
+        self._videos[HASH_NULL]     = self.msg.sender
+        self._adrs[HASH_NULL]       = self.msg.sender
+        self._ad_video[HASH_NULL]   = HASH_NULL
 
     def on_update(self) -> None:
         super().on_update()
@@ -124,6 +137,7 @@ class LiveBroadcast(IconScoreBase, TokenStandard):
 
         Logger.debug(f'Transfer({_from}, {_to}, {_value}, {_data})', TAG)
 
+    # Live Broadcast
     @external(readonly=True)
     def ownerOf(self, _hash: str) -> Address:
         return self._videos[_hash]
@@ -135,15 +149,75 @@ class LiveBroadcast(IconScoreBase, TokenStandard):
     def _upload(self, _hash: str, _owner: Address):
         self._videos[_hash] = _owner
 
-    # donation - auto-distribution
     @external
     def donation(self, _hash: str, _value: int, _data: bytes = None):
         if _data is None:
             _data = b'None'
 
-        _from = self.msg.sender
-        _to = self._videos[_hash]
+        _from   = self.msg.sender
+        _to     = self.ownerOf(_hash)
+
+
+        # Checks valid hash
+        # Do not need do this: Duplicated at DictDB method.
+        """
+        if _to is None:
+            revert("Unvalid video hash: Key is None")
+        """
+
+        # Checks the sending value and balance.
+        # Do not need do this: Duplicated at '_transfer' function.
+        """
+        if _value < 0:
+            revert("Transferring value cannot be less than zero")
+        if self._balances[_from] < _value:
+            revert("Out of balance")
+        """
+
+        # to platform admin.
         _fee = int(_value / 10)
 
         self._transfer(_from, _to, (_value - _fee), _data)
+        self._transfer(_from, self._admin, _fee, _data)
+
+    # Live Commerce
+    @external(readonly=True)
+    def adrOf(self, _hash: str) -> Address:
+        return self._adrs[_hash]
+
+    @external
+    def videoOf(self, _hash: str) -> str:
+        return self._ad_video[_hash]
+
+    @external
+    def enroll(self, _hash: str):
+        # advertiser
+        self._enroll(_hash, self.msg.sender)
+
+    def _enroll(self, _hash: str, _owner: Address):
+        self._adrs[_hash] = _owner
+
+    @external
+    def place(self, _where: str, _hash: str):
+        # prerequisite: upload() and enroll() first.
+        if self.adrOf(_hash) != self.msg.sender and self.ownerOf(_where) != self.msg.sender:
+            revert("Not Permitted")
+
+        self._ad_video[_hash] = _where
+
+    @external
+    def purchase(self, _hash: str, _value: int, _data: bytes = None):
+        if _data is None:
+            _data = b'None'
+
+        _from       = self.msg.sender
+        _adr        = self.adrOf(_hash)
+        _creator    = self.ownerOf(self.videoOf(_hash))
+
+        _fee            = int(_value / 10)
+        _adr_share      = int((_value - _fee) / 2)
+        _creator_share  = _value - _fee - _adr_share
+
+        self._transfer(_from, _adr, _adr_share, _data)
+        self._transfer(_from, _creator, _creator_share, _data)
         self._transfer(_from, self._admin, _fee, _data)
